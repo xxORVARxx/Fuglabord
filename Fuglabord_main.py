1c171db07a5915b969a9ac072efb9d007a4b8fea
xxORVARxx <xxorvarxx@gmail.com>
2020-03-11 21:32:33 +0000

    Testing the GPIO-pins

--- Fuglabord_main.py
+++ Fuglabord_main.py
@@ -22,3 +22,50 @@ try:
 except RuntimeError:
     print("""Error importing RPi.GPIO! This is probably because you need superuser privileges.  
     You can achieve this by using 'sudo' to run your script""")
+    
+# Using the BOARD numbering system. This refers to the pin numbers on the P1 header of
+# the Raspberry Pi board.
+GPIO.setmode(GPIO.BOARD)
+
+
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def Callback_22(channel):
+    print("Callback_22", channel, GPIO.input(22))
+
+def Callback_24():
+    print("Callback_24")
+
+def Callback_26():
+    print("Callback_26")
+
+    
+GPIO.add_event_detect(22, GPIO.BOTH)
+GPIO.add_event_detect(24, GPIO.BOTH)
+GPIO.add_event_detect(26, GPIO.BOTH)
+ 
+GPIO.add_event_callback(22, Callback_22)
+GPIO.add_event_callback(24, Callback_24)
+GPIO.add_event_callback(26, Callback_26)
+
+
+try:
+    print("Loop.")
+    while(True):
+        time.sleep(0.2)
+except:
+    GPIO.cleanup()
+    print("Cleanup.")
+
+
+
+
+"""
+GPIO.output(5, GPIO.LOW)
+for i in range(5):
+    GPIO.output(5, not GPIO.input(5))
+    print("Working", i)
+    time.sleep(3)
+"""
